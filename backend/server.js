3c427fe26b8edd6bf3558a3aa9d3ef99ae7b0f15
Leibiuc-Darius-30127 <69507168+Leibiuc@users.noreply.github.com>
2022-03-23 14:56:38 +0200

    connected to DB

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,28 @@
-console.log("merge echipa")
-console.log("Eu sunt Ale")
-console.log('eU SUNT Ale')
-//sadasdas
+const express = require('express')
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose')
+const cors = require('cors');
+
+
+
+mongoose.connect('mongodb+srv://sinus2:sinusdoi@cluster0.j4zlt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => {
+    console.log('connected to database Dashboard (0_0)')
+  }).catch((err) => {
+    console.log('connection failed (dashboard)'),
+    console.log(err)
+  });
+
+const app = express();
+
+app.use(cors({origin: true, credentials: true}));
+app.use(bodyparser.json({limit: '400mb'}));
+app.use(bodyparser.urlencoded({limit: '400mb', extended: true}));
+
+
+app.listen(8080);
