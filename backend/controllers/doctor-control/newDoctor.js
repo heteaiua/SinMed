7c879a8e764ae87a7d30ed44c646e46c9c9e5d64
Leibiuc-Darius-30127 <69507168+Leibiuc@users.noreply.github.com>
2022-04-26 15:24:07 +0300

    updated newDoctor function

--- backend/controllers/doctor-control/newDoctor.js
+++ backend/controllers/doctor-control/newDoctor.js
@@ -5,6 +5,7 @@
 //const bcrypt = require('bcryptjs')
 //const jwt = require('jsonwebtoken')
 const Doctor = require("../../models/doctor");
+const Specialization = require("../../models/specialization");
 
 const createDoctor = async (req, res, next) => {
   // const errors = validationResult(req);
@@ -22,18 +23,38 @@ const createDoctor = async (req, res, next) => {
     email,
     password,
     cnp,
-    rating,
     gender,
     age,
   } = req.body;
 
   let createdDoctor;
   try {
+
     const doctors = await Doctor.find().exec();
+
+    const specializations = await Specialization.find().exec();
+    const specialization = specializations.filter(s => {
+        return s.id === idSpecialization;
+    })
+    if (specialization.length === 0){
+        console.log(specialization)
+        return res.json({
+            message: " Invalid specialization!"
+        });
+      }
+    
+
     const usedEmail = doctors.find((p) => p.email === email);
     const usedCNP = doctors.find((p) => p.cnp === cnp);
     console.log(usedEmail);
     console.log(usedCNP);
+    
+    // if (!specialization)
+    // // se verifica daca cnp-ul introdus exista in baza de date
+    // return res.json({
+    //   message: " Invalid specialization! ",
+    // });
+
     if (usedCNP)
       // se verifica daca cnp-ul introdus exista in baza de date
       return res.json({
@@ -60,7 +81,9 @@ const createDoctor = async (req, res, next) => {
     //     error: "Could not create user."
     //   })
     // }
+ 
 
+ 
     createdDoctor = new Doctor({
       idSpecialization,
       firstName,
@@ -68,10 +91,10 @@ const createDoctor = async (req, res, next) => {
       email,
       password,
       cnp,
-      rating,
       gender,
       age,
     });
+    console.log(createdDoctor)
     await createdDoctor.save();
   } catch (err) {
     res.status(500).json("Registration has failed!");
@@ -87,9 +110,9 @@ const createDoctor = async (req, res, next) => {
   // } catch (err) {
   //   return res.status(500).json("Failed registration!");
   // }
-  res.status(201).json({
+  res.json({
     message: "New doctor added!",
-    doctors: createdDoctor,
+    doctor: createdDoctor,
     // token: token,
   });
 };
