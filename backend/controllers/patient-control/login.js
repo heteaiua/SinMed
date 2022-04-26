b9158a479b85fe81b24a6da3713992ae9411957d
Leibiuc-Darius-30127 <69507168+Leibiuc@users.noreply.github.com>
2022-04-26 14:15:43 +0300

    login function done

---
+++ backend/controllers/patient-control/login.js
@@ -0,0 +1,69 @@
+//const bcrypt = require('bcryptjs')
+//const jwt = require('jsonwebtoken')
+const Patient = require("../../models/patient");
+
+const loginPatient = async (req, res, next) => {
+  //functie care logheaza un utilizator existent cerand un email si o parola
+
+  const { email, password } = req.body;
+
+  let existingPatient;
+  try {
+    existingPatient = await Patient.findOne({
+      email: email,
+    }); //transformarea continutului din baza de date intr-un array de obiecte
+
+    if (!existingPatient)
+      // se verifica daca email-ul introdus exista in baza de date si daca corespunde cu parola
+      return res.status(401).json({
+        message: "No account found .",
+      }); //mesaj de eroare , nu se mai executa functia in continuare
+  } catch (error) {
+    return res.json({
+      error,
+    });
+  }
+
+
+  try {
+    if (existingPatient.password !== password)
+      // se verifica daca email-ul introdus exista in baza de date si daca corespunde cu parola
+      return res.json({
+        message: "Wrong password! ",
+      }); //mesaj de eroare , nu se mai executa functia in continuare
+  } catch (error) {
+    return res.json({
+      error,
+    });
+  }
+
+  // if (existingPatient.loggedIn)
+  //   return res.status(401).json("User already logged in!")
+
+  // let isValidPassword = false;
+  // try {
+  //   isValidPassword = await bcrypt.compare(password, existingPatient.password)
+  // } catch (error) {
+  //   return res.status(500).json('Login failed, please try again later.')
+  // }
+
+  // if (!isValidPassword) {
+  //   return res.status(401).json('Wrong password.')
+  // }
+
+  // let token;
+  // try {
+  //   token = jwt.sign({ userID: existingPatient.id, name: existingPatient.name }, 'super_secret', { expiresIn: '5h' });
+  // } catch (err) {
+  //   return res.status(500).json("Login failed!")
+  // }
+  // existingPatient.loggedIn = true;
+
+  res.json({
+    message: "Welcome back, " + existingPatient.firstName + "!",
+    patient: existingPatient,
+    // token: token
+  }); // mesaj in caz de succes
+};
+
+module.exports = loginPatient;
