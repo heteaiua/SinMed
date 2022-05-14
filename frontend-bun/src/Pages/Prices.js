475734bf04b965389a63ff56e440ff11fd2436ac
hetea-ioana-alina-30127 <44019689+heteaiua@users.noreply.github.com>
2022-05-14 15:58:06 +0300

    Prices Page Frontend

---
+++ frontend-bun/src/Pages/Prices.js
@@ -0,0 +1,231 @@
+import React from "react";
+import "../Pages/Prices.css";
+
+const Prices = (props) => {
+  return (
+    <>
+      <section>
+        <h1>preturi Computer tomograf (CT)</h1>
+        <div class="tbl-header">
+          <table cellpadding="0" cellspacing="0" border="0">
+            <thead>
+              <tr>
+                <th>CT</th>
+                <th>Nativ</th>
+                <th>Contrast</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+        <div class="tbl-content">
+          <table cellpadding="0" cellspacing="0" border="0">
+            <tbody>
+              <tr>
+                <td>CT cranian</td>
+                <td>300 lei</td>
+                <td>400 lei</td>
+              </tr>
+              <tr>
+                <td>CT regiune cervicală</td>
+                <td>300 lei</td>
+                <td>400 lei</td>
+              </tr>
+              <tr>
+                <td>CT cranio-cervical</td>
+                <td>300 lei</td>
+                <td>400 lei</td>
+              </tr>
+              <tr>
+                <td>CT torace</td>
+                <td>300 lei</td>
+                <td>340 lei</td>
+              </tr>
+              <tr>
+                <td>CT abdomen</td>
+                <td>270 lei</td>
+                <td>400 lei</td>
+              </tr>
+              <tr>
+                <td>CT pelvis</td>
+                <td>300 lei</td>
+                <td>400 lei</td>
+              </tr>
+              <tr>
+                <td>CT coloană vertebrală (segment)</td>
+                <td>300 lei</td>
+                <td>330 lei</td>
+              </tr>
+              <tr>
+                <td>CT membru/segment (sup., inferior)</td>
+                <td>300 lei</td>
+                <td>400 lei</td>
+              </tr>
+              <tr>
+                <td>CT toraco-abdomeno-pelvin</td>
+                <td>300 lei</td>
+                <td>400 lei</td>
+              </tr>
+              <tr>
+                <td>CT abdomeno-pelvin</td>
+                <td>200 lei</td>
+                <td>400 lei</td>
+              </tr>
+              <tr>
+                <td>CT abdomeno-pelvin + reconstrucții coloană</td>
+                <td>300 lei</td>
+                <td>350 lei</td>
+              </tr>
+              <tr>
+                <td>CT ureche (internă, medie)</td>
+                <td>300 lei</td>
+                <td>420 lei</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
+
+      <section>
+        <h1>PREȚURI REZONANȚĂ MAGNETICĂ NUCLEARĂ (RMN)</h1>
+        <div class="tbl-header">
+          <table cellpadding="0" cellspacing="0" border="0">
+            <thead>
+              <tr>
+                <th>RMN</th>
+                <th>Nativ</th>
+                <th>Contrast</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+        <div class="tbl-content">
+          <table cellpadding="0" cellspacing="0" border="0">
+            <tbody>
+              <tr>
+                <td>IRM coloana vertebrala-1 regiune</td>
+                <td>300 lei</td>
+                <td>400 lei</td>
+              </tr>
+              <tr>
+                <td>IRM coloana vertebrala-2 regiuni</td>
+                <td>300 lei</td>
+                <td>400 lei</td>
+              </tr>
+              <tr>
+                <td>IRM coloana vertebrala in totalitate</td>
+                <td>300 lei</td>
+                <td>400 lei</td>
+              </tr>
+              <tr>
+                <td>IRM coloana lombara+ articulatii coxo-femurale(sold)</td>
+                <td>300 lei</td>
+                <td>340 lei</td>
+              </tr>
+              <tr>
+                <td>IRM coloana lombara+ articulatii sacro-iliace</td>
+                <td>270 lei</td>
+                <td>400 lei</td>
+              </tr>
+              <tr>
+                <td>IRM articulatii sacro-iliace</td>
+                <td>300 lei</td>
+                <td>400 lei</td>
+              </tr>
+              <tr>
+                <td>IRM articulatii coxo-femurale(sold)</td>
+                <td>300 lei</td>
+                <td>330 lei</td>
+              </tr>
+              <tr>
+                <td>IRM genunchi</td>
+                <td>300 lei</td>
+                <td>400 lei</td>
+              </tr>
+              <tr>
+                <td>IRM genunchi bilateral</td>
+                <td>300 lei</td>
+                <td>400 lei</td>
+              </tr>
+              <tr>
+                <td>
+                  IRM extremitati/segment unilateral
+                  (antebrat,coapsa,gamba,picior)
+                </td>
+                <td>200 lei</td>
+                <td>400 lei</td>
+              </tr>
+              <tr>
+                <td>
+                  IRM extremitati/segment bilateral
+                  (antebrat,coapsa,gamba,picior)
+                </td>
+                <td>300 lei</td>
+                <td>350 lei</td>
+              </tr>
+              <tr>
+                <td>IRM abdomen+ pelvis</td>
+                <td>300 lei</td>
+                <td>420 lei</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
+
+      <section>
+        <h1>PREȚURI EXAMINARI MEDICALE</h1>
+        <div class="tbl-header">
+          <table cellpadding="0" cellspacing="0" border="0">
+            <thead>
+              <tr>
+                <th>Denumirea examinarii</th>
+                <th>Pret</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+        <div class="tbl-content">
+          <table cellpadding="0" cellspacing="0" border="0">
+            <tbody>
+              <tr>
+                <td>Pachet reumatologie – consult+ecografie</td>
+                <td>330 lei</td>
+              </tr>
+              <tr>
+                <td>
+                  Test alergeni inhalatori, alimentari, epicutani, medicamente
+                </td>
+                <td>100 lei</td>
+              </tr>
+              <tr>
+                <td>Imunoterapia</td>
+                <td>100 lei</td>
+              </tr>
+              <tr>
+                <td>Ecografie abdominală</td>
+                <td>220 lei</td>
+              </tr>
+              <tr>
+                <td>Ecografie cardiacă</td>
+                <td>270 lei</td>
+              </tr>
+              <tr>
+                <td>Ecografie părțI moi</td>
+                <td>220 lei</td>
+              </tr>
+              <tr>
+                <td>Ecografie dopller vasculară</td>
+                <td>330 lei</td>
+              </tr>
+              <tr>
+                <td>Adeverinta neurologie</td>
+                <td>100 lei</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
+    </>
+  );
+};
+export default Prices;
