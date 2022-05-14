69b57e63980a8640c42227c582b970f844d8ea6c
alexandra-pop-05 <ale_ale.0805@yahoo.com>
2022-05-14 13:23:26 +0300

    greseala

--- frontend-bun/src/App.js
+++ frontend-bun/src/App.js
@@ -26,7 +26,6 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/Appointments/:doctorId" element={<NewAppointment />}/>
           <Route path="/Appointments" element={<Appointments />}/>
-=======
           <Route path="/contact" element={<Contact />} />
 
         </Routes>
