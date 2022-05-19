6c12c2e2fc454b82e6e083fc1d4bfd4881c4fa21
cereteu-tudor-30127 <cereteu.tudor21@gmail.com>
2022-05-19 23:32:50 +0300

    cleaned the code

--- frontend-bun/src/App.js
+++ frontend-bun/src/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
-import Title from "./Components/Title";
-import Menu from "./Components/Menu";
 import Sidebar from "./Components/Navigation/Sidebar";
 import Home from "./Pages/Home";
 import Specializations from "./Pages/Specializations";
@@ -14,7 +12,6 @@ import Appointments from "./Pages/Appointments";
 import Contact from "./Pages/Contact";
 import AboutUs from "./Pages/AboutUs";
 import AuthContext from "./Context/auth-context";
-import SpecializationItem from "./Components/SpecializationItem";
 import DoctorsListBySpecialization from "./Pages/DoctorsListBySpecialization";
 import Prices from "./Pages/Prices";
 
@@ -34,52 +31,6 @@ function App() {
     setPatientId(null);
   }, []);
 
-  let routes;
-
-  if (isLoggedIn) {
-  routes = (
-      <Routes>
-        <Route path="/Home" element={<Home />} />
-        <Route path="/Specializations" element={<Specializations />} />
-        <Route path="/Doctors" element={<Doctors />} />
-        <Route path="/Appointments/:doctorId" element={<NewAppointment />} />
-        <Route path="/Appointments" element={<Appointments />} />
-        <Route path="/contact" element={<Contact />} />
-        {/* <Route path="/login" element={<Login />} /> */}
-
-        {/* <Navigate to="/Home" /> */}
-      </Routes>
-    );
-  } else {
-    routes = (
-      <Routes>
-        <Route path="/Home" element={<Home />} />
-        <Route path="/Specializations" element={<Specializations />} />
-        <Route path="/Doctors" element={<Doctors />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/Appointments" element={<Appointments />} />
-        <Route path="/contact" element={<Contact />} />
-        {/* <Route
-          path="/Appointments/:doctorId"
-          element={<Navigate replace to="login" />}
-        /> */}
-        {/* vreau aici cand apas pe Programare sa ma redirectioneze la Login */}
-      </Routes>
-    );
-  }
-  // return (
-  //   <AuthContext.Provider
-  //     value={{
-  //       isLoggedIn: isLoggedIn,
-  //       patientId: patientId,
-  //       login: login,
-  //       logout: logout,
-  //     }}
-  //   >
-  //     <main>{routes}</main>
-  //   </AuthContext.Provider>
-  // );
   return (
     <div className="App">
       <AuthContext.Provider
@@ -113,21 +64,6 @@ function App() {
           </Routes>
         </Router>
       </AuthContext.Provider>
-
-      {/* <Router>
-        <Sidebar />
-        <Routes>
-          <Route path="/Home" element={<Home />} />
-          <Route path="/Specializations" element={<Specializations />} />
-          <Route path="/Doctors" element={<Doctors />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/Appointments/:doctorId" element={<NewAppointment />} />
-          <Route path="/Appointments" element={<Appointments />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/About_Us" element={<AboutUs />} />
-        </Routes>
-      </Router> */}
     </div>
   );
 }
