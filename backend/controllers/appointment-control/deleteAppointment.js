c23242be4bee492f5de20b40dab629389dbe976d
Leibiuc-Darius-30127 <69507168+Leibiuc@users.noreply.github.com>
2022-05-29 10:19:51 +0300

    deleteAppointment

---
+++ backend/controllers/appointment-control/deleteAppointment.js
@@ -0,0 +1,20 @@
+const Appointment = require('../../models/appointment');
+const deleteAppointment = async (req, res, next) => {
+    const appointmentID = req.params.aid;
+
+    let appointment;
+    try {
+        appointment = await Appointment.findById(appointmentID);
+    } catch (err) {
+        return res.status(500).json("Deleting appointment failed! ")
+    }
+
+    try {
+        await appointment.remove()
+    } catch (err) {
+        return res.status(500).json({ message: "Deleting appointment has failed! ", error: err })
+    }
+    return res.status(200).json({ message: "Appointment card deleted.", appointment: appointment })
+}
+
+module.exports = deleteAppointment
--- backend/routes/appointment-routes.js
+++ backend/routes/appointment-routes.js
@@ -3,12 +3,14 @@ const showAllAppointmentsByUserID = require("../controllers/appointment-control/
 
 const showAllAppointments = require("../controllers/appointment-control/getAppointmentsByUserID");
 const createAppointment = require("../controllers/appointment-control/newAppointment");
+const deleteAppointment = require("../controllers/appointment-control/deleteAppointment");
 
 const router = express.Router();
 
 router.post("/:pid/:did", createAppointment);
 router.get("/", showAllAppointments);
 router.get("/:idPatient", showAllAppointmentsByUserID);
+router.delete("/:aid", deleteAppointment);
 
 
 module.exports = router;
